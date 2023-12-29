6930c11bee6a48eafb66102ea75877473f09485f
Tadminster <91bottlenine@naver.com>
2023-12-29 14:31:46 +0900

    cppMacro에 GetClass, GetAssetDynamic 추가

--- Source/SGA1/CppMacro.h
+++ Source/SGA1/CppMacro.h
@@ -4,7 +4,7 @@
 namespace CppMacro
 {
 	template<typename T>
-	static void CreateComponet(AActor* Actor, T*& Component, const FName Name, USceneComponent* Parent = nullptr)
+	static void CreateComponet(AActor* Actor, T*& Component, const FName& Name, USceneComponent* Parent = nullptr)
 	{
 		Component = Actor->CreateDefaultSubobject<T>(Name);
 
@@ -12,13 +12,35 @@ namespace CppMacro
 		else Actor->SetRootComponent(Component);
 	}
 
+	template<typename T>
+	static void GetClass(TSubclassOf<T>* OutClass, const FString& Path)
+	{
+		ConstructorHelpers::FClassFinder<T> asset(*Path);
+
+		//verifyf(asset.Succeeded(), L"Succeeded")
+		if (asset.Succeeded()) *OutClass = asset.Class;
+		else UE_LOG(LogTemp, Warning, TEXT("Failed to Get Class: %s"), *Path);
+	}
+
 	static void GetAsset(UStaticMesh*& Asset, const FString& Path)
 	{
 		static ConstructorHelpers::FObjectFinder<UStaticMesh> asset(*Path);
 		
-		if (asset.Succeeded())
-		{
-			Asset = asset.Object;
-		}
+		//verifyf(asset.Succeeded(), L"Succeeded")
+		if (asset.Succeeded()) Asset = asset.Object;
+		else UE_LOG(LogTemp, Warning, TEXT("Failed to Get Asset: %s"), *Path);
+	}
+
+	template<typename T>
+	static void GetAssetDynamic(T*& AssetObject, const FString& Path)
+	{
+		T obj = Cast<T>(StaticLoadObject(T::StaticClass(), nullptr, *Path));
+
+		//verifyf(obj, L"Succeeded")
+		if (obj) AssetObject = obj;
+		else UE_LOG(LogTemp, Warning, TEXT("Failed to Get AssetDynamic: %s"), *Path);
 	}
 }
+
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
